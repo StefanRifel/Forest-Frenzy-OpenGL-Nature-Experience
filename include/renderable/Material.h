7966d12e5d8b3c1318d8f8774e10dfe3e8ad761d
Stefan Rifel <s.rifel23i@gmail.com>
2024-06-16 12:15:08 +0200

    feat: add new attribute fields to material

--- include/renderable/Material.h
+++ include/renderable/Material.h
@@ -3,12 +3,20 @@
 
 #include "../../libs/SMath/include/vec3.hpp"
 
-typedef struct {
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
-    float shininess;
-} Material;
+class Material {
+public:
+    std::string mtlName;
+    vec3 emissive;    ///< Ke
+    vec3 ambient;     ///< Ka
+    vec3 diffuse;     ///< Kd
+    vec3 specular;    ///< Ks
+    float shininess;  ///< Ns
+
+    float density;    ///< Ni
+    float dissolve;   ///< d
+
+    float illumination; ///< illum
+};
 
 enum MaterialType {
     MATERIAL_GOLD,
@@ -21,42 +29,72 @@ enum MaterialType {
 };
 
 const Material materials[MATERIAL_COUNT] = {
-    {
-            vec3(0.24725f, 0.1995f, 0.0745f),
-            vec3(0.75164f, 0.60648f, 0.22648f),
-            vec3(0.628281f, 0.555802f, 0.366065f),
-            51.2f
-    },
-    {
-            vec3(0.19225f, 0.19225f, 0.19225f),
-            vec3(0.50754f, 0.50754f, 0.50754f),
-            vec3(0.508273f, 0.508273f, 0.508273f),
-            51.2f
-    },
-    {
-            vec3(0.2125f, 0.1275f, 0.054f),
-            vec3(0.714f, 0.4284f, 0.18144f),
-            vec3(0.393548f, 0.271906f, 0.166721f),
-            25.6f
-    },
-    {
-            vec3(0.1745f, 0.01175f, 0.01175f),
-            vec3(0.61424f, 0.04136f, 0.04136f),
-            vec3(0.727811f, 0.626959f, 0.626959f),
-            76.8f
-    },
-    {
-            vec3(0.135f, 0.2225f, 0.1575f),
-            vec3(0.54f, 0.89f, 0.63f),
-            vec3(0.316228f, 0.316228f, 0.316228f),
-            12.8f
-    },
-    {
-            vec3(0.0f, 0.0f, 0.0f),
-            vec3(0.01f, 0.01f, 0.01f),
-            vec3(0.50f, 0.50f, 0.50f),
-            32.0f
-    }
+        {
+                "Gold",
+                vec3(0.0f, 0.0f, 0.0f), // emissive
+                vec3(0.24725f, 0.1995f, 0.0745f), // ambient
+                vec3(0.75164f, 0.60648f, 0.22648f), // diffuse
+                vec3(0.628281f, 0.555802f, 0.366065f), // specular
+                51.2f, // shininess
+                1.0f, // density
+                1.0f, // dissolve
+                2.0f // illumination
+        },
+        {
+                "Silver",
+                vec3(0.0f, 0.0f, 0.0f), // emissive
+                vec3(0.19225f, 0.19225f, 0.19225f), // ambient
+                vec3(0.50754f, 0.50754f, 0.50754f), // diffuse
+                vec3(0.508273f, 0.508273f, 0.508273f), // specular
+                51.2f, // shininess
+                1.0f, // density
+                1.0f, // dissolve
+                2.0f // illumination
+        },
+        {
+                "Bronze",
+                vec3(0.0f, 0.0f, 0.0f), // emissive
+                vec3(0.2125f, 0.1275f, 0.054f), // ambient
+                vec3(0.714f, 0.4284f, 0.18144f), // diffuse
+                vec3(0.393548f, 0.271906f, 0.166721f), // specular
+                25.6f, // shininess
+                1.0f, // density
+                1.0f, // dissolve
+                2.0f // illumination
+        },
+        {
+                "Ruby",
+                vec3(0.0f, 0.0f, 0.0f), // emissive
+                vec3(0.1745f, 0.01175f, 0.01175f), // ambient
+                vec3(0.61424f, 0.04136f, 0.04136f), // diffuse
+                vec3(0.727811f, 0.626959f, 0.626959f), // specular
+                76.8f, // shininess
+                1.0f, // density
+                0.55f, // dissolve
+                2.0f // illumination
+        },
+        {
+                "Jade",
+                vec3(0.0f, 0.0f, 0.0f), // emissive
+                vec3(0.135f, 0.2225f, 0.1575f), // ambient
+                vec3(0.54f, 0.89f, 0.63f), // diffuse
+                vec3(0.316228f, 0.316228f, 0.316228f), // specular
+                12.8f, // shininess
+                1.0f, // density
+                0.95f, // dissolve
+                2.0f // illumination
+        },
+        {
+                "Black Plastic",
+                vec3(0.0f, 0.0f, 0.0f), // emissive
+                vec3(0.0f, 0.0f, 0.0f), // ambient
+                vec3(0.01f, 0.01f, 0.01f), // diffuse
+                vec3(0.50f, 0.50f, 0.50f), // specular
+                32.0f, // shininess
+                1.0f, // density
+                1.0f, // dissolve
+                2.0f // illumination
+        }
 };
 
 #endif
