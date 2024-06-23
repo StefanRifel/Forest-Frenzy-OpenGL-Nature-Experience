d74d3902487f6a3f47cec2ce92b7d197d1da4401
Stefan Rifel <s.rifel23i@gmail.com>
2024-06-23 08:41:16 +0200

    feat: enable anti aliasing

--- src/context/OpenGLContext.cpp
+++ src/context/OpenGLContext.cpp
@@ -40,6 +40,7 @@ bool OpenGLContext::init(Window* pWindow) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_SAMPLES, 4);
 
     // Initialize GLFWWindow
     pWindow->window = glfwCreateWindow(pWindow->width, pWindow->height, pWindow->programName.c_str(), nullptr, nullptr);
